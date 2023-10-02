ccfefce299dc3ce20dbcd6fd9d7c5f7b3729be98
maratdev <voredev@gmail.com>
2023-10-02 16:15:30 +0300

    feat(dev) redesign Main

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,16 +1,7 @@
 import Promo from '../Promo/Promo.jsx';
-import AboutProject from '../AboutProject/AboutProject.jsx';
-import Techs from '../Techs/Techs.jsx';
-import AboutMe from '../AboutMe/AboutMe.jsx';
-import Portfolio from '../Portfolio/Portfolio.jsx';
-
 const Main = () => (
   <main>
     <Promo/>
-    <AboutProject/>
-    <Techs/>
-    <AboutMe/>
-    <Portfolio/>
   </main>
 );
 
--- src/components/Promo/Promo.jsx
+++ src/components/Promo/Promo.jsx
@@ -1,9 +1,10 @@
 import './Promo.css';
-
+import logo from "../../images/a.svg";
 const Promo = () => (
   <section className="Promo">
     <div className="Promo__wrap">
-      <h1 className="Promo__title list">Учебный проект студента факультета Веб-разработки.</h1>
+      <h1 className="Promo__title list"> <img className="Promo__title_img" src={logo} alt="кинохлопушка"/> Movies Explorer | BeatFilm</h1>
+      <p className="Promo__info">Сервис, в котором можно найти фильмы по запросу и сохранить в личном кабинете.</p>
     </div>
   </section>
 );
